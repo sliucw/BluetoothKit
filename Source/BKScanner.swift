6734eae09643e238518ec577f9d11a849c555e1f
sliucw <sliucw@gmail.com>
2020-08-11 14:36:21 +0800

    feat: 让蓝牙扫描所有设备

--- Source/BKScanner.swift
+++ Source/BKScanner.swift
@@ -56,8 +56,9 @@ internal class BKScanner: BKCBCentralManagerDiscoveryDelegate {
             busy = true
             scanHandlers = ( progressHandler: progressHandler, completionHandler: completionHandler)
             let options = [CBCentralManagerScanOptionAllowDuplicatesKey: updateDuplicates]
-            centralManager.scanForPeripherals(withServices: configuration.serviceUUIDs, options: options)
-            if(duration > 0) {
+            //centralManager.scanForPeripherals(withServices: configuration.serviceUUIDs, options: options)
+            centralManager.scanForPeripherals(withServices: nil, options: options)
+            if duration > 0 {
                 durationTimer = Timer.scheduledTimer(timeInterval: duration, target: self, selector: #selector(BKScanner.durationTimerElapsed), userInfo: nil, repeats: false)
             }
         } catch let error {
@@ -107,7 +108,7 @@ internal class BKScanner: BKCBCentralManagerDiscoveryDelegate {
 
     // MARK: BKCBCentralManagerDiscoveryDelegate
 
-    internal func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    internal func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
         guard busy else {
             return
         }
@@ -117,8 +118,7 @@ internal class BKScanner: BKCBCentralManagerDiscoveryDelegate {
         let discovery = BKDiscovery(advertisementData: advertisementData, remotePeripheral: remotePeripheral, RSSI: RSSI)
         if let index = discoveries.firstIndex(of: discovery) {
             discoveries[index] = discovery
-        }
-        else {
+        } else {
             discoveries.append(discovery)
         }
         scanHandlers?.progressHandler?([ discovery ])
